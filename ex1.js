579215be514db80b3146de94cac1213287162534
unknown <dburtonthompson@gmail.com>
2020-04-08 15:36:30 -0400

    sandwich calc

---
+++ ex1.js
@@ -0,0 +1,18 @@
+function sandwichcalc(slicesOfBread, SlicesOfCheese){
+	var = sandwiches = slicesOfBread / 2;
+alert(sandwiches);
+	if((slicesOfBread > 1) && (SlicesOfCheese > 0) && ( (SlicesOfCheese < sandwiches)) {
+			sandwiches = SlicesOfCheese;
+			console.log(sandwiches);
+			return sandwiches;
+		} else if (SlicesOfCheese >= sandwiches) && (slicesOfBread > 1){
+			console.log(sandwiches);
+			return sandwiches;
+		} else if ((SlicesOfCheese = 0) || (slicesOfBread < 2)) {
+			console.log("No sandwiches!")
+			return 0;
+		}
+
+	
+}
+sandwichcalc(10, 1);
